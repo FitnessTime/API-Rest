4b87762d19dc1a212c0ad3c7ade385bf54cae45d
lmoscheni <leandromoscheni@gmail.com>
2016-03-26 17:37:46 -0300

    Ajusto algo de las exceptions

--- app/controllers/SesionService.rb
+++ app/controllers/SesionService.rb
@@ -1,19 +1,16 @@
 FitnessTimeApi::App.controllers :sesionService do
-  
+
   get :iniciarSesion, :map => '/login' do
-    usuario = Usuario.find_by_email(params[:email])
-      
-    if usuario == nil
-      #headers['X-Forwarded-For'] = request['X-Forwarded-For']
+    begin
+      usuario = Usuario.get(params[:email])
+      securityToken = SecurityToken.new(usuario.email,usuario.nombre,generate_random)
+      securityToken.save
+      get_sucsses_response(securityToken.to_json)
+    rescue RuntimeError
       get_error_response(404, "No existe el usuario")
-    else if usuario.is_the_same_password?(params[:pass])
-          securityToken = SecurityToken.new(usuario.email,usuario.nombre,generate_random)
-          securityToken.save
-          get_sucsses_response(securityToken.to_json)
-        else
-          get_error_response(404, "La contrasenia es incorrecta")
-        end
-      end
+    rescue DifferentPasswordError
+      get_error_response(404, "La contrasenia es incorrecta")
+    end
   end
 
   get :cerrarSesion, :map => '/close' do
