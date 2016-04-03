79b6321c9ad6d3ce4d2f6c51a0f10567efd7d12a
lmoscheni <leandromoscheni@gmail.com>
2016-04-03 19:12:40 -0300

    Modifico el helper de Rutina

--- app/helpers/RutinaHelper.rb
+++ app/helpers/RutinaHelper.rb
@@ -1,13 +1,32 @@
 FitnessTimeApi::App.helpers do
 
   def actualizar_rutina(rutina,params)
-    # Podriamos unificar las dos rutinas en una unica clase, estoy viendo que es
-    # al pedo tenerlas dividas
-    if(rutina.rutina_de_carga?)
-      actualizar_rutina_de_carga(rutina,params)
-    else
-      actualizar_rutina_de_aerobico(rutina,params)
-    end
+    update(:inicio => params[:inicio], :fin => params[:fin],
+    			   :descripcion => params[:descripcion], :aclaracion => params[:aclaracion])
+    # if(rutina.rutina_de_carga?)
+    #   actualizar_rutina_de_carga(rutina,params)
+    # else
+    #   actualizar_rutina_de_aerobico(rutina,params)
+    # end
   end
+
+  def create_rutina(params)
+    rutina = Rutina.new()
+    rutina.inicio = params[:inicio]
+  	rutina.fin = params[:fin]
+    rutina.descripcion = params[:descripcion]
+    rutina.aclaracion = params[:aclaracion]
+    rutina.rutina_de_carga?= params[:rutina_de_carga?]
+    rutina.save!()
+  end
+
+  # def actualizar_rutina_de_carga(rutina,params)
+  #   update(:inicio => params[:inicio], :fin => params[:fin],
+  #   			   :descripcion => params[:descripcion], :aclaracion => params[:aclaracion])
+  # end
+  #
+  # def actualizar_rutina_de_aerobico(rutina,params)
+  #
+  # end
 
 end
