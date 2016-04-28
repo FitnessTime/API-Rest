92439efd1a6d4001ee82bb81608b3e59df63cbd4
skalic.julian <skalic.julian@gmail.com>
2016-04-27 23:10:16 -0300

    .

--- app/controllers/SesionService.rb
+++ app/controllers/SesionService.rb
@@ -10,10 +10,10 @@ FitnessTimeApi::App.controllers :sesionService do
         securityToken.save()
         get_success_response(securityToken.to_json)
       else
-        get_error_response(404, "La contrasenia es incorrecta.".to_json)
+        get_error_response(404, "La contrasenia es incorrecta.")
       end
     rescue DataMapper::ObjectNotFoundError => e
-      get_error_response(404, "No existe el usuario".to_json)
+      get_error_response(404, "No existe el usuario")
     end
   end
 
