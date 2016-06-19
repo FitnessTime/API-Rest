6e13b504d4e08a077e56627da2d5a7a3f31ce665
skalic.julian <skalic.julian@gmail.com>
2016-06-19 16:58:49 -0300

    Agregar marcas

--- app/controllers/MarcaService.rb
+++ app/controllers/MarcaService.rb
@@ -1,27 +1,19 @@
 FitnessTimeApi::App.controllers :marcaService do
   
   post :registrarMarca, :map => '/marcas' do
-    # Verificamos que se pueda realizar la operacion
-    marca = create_marca(params)
-    marca.save()
-    #Comunicamos el resultado de la operacion y mandamos el json
+    securityToken = SecurityToken.find_by_authToken(params[:authToken])
+    if securityToken == nil
+      get_error_response(404,"Usuario no autorizado.")
+    else
+      jsonMarca = JSON.parse(params[:marca])
+      ejercicio = EjercicioDeCarga.find_by_id(jsonMarca['idEjercicio'])
+      ejercicio = create_ejercicio(jsonMarca, ejercicio)
+    end
   end
 
   get :marcas, :map => '/rutinas/:rutina_id/ejercicios/:ejercicio_id/marcas' do
     # Verificamos que se pueda realizar la operacion
     @marcas = Ejercicio.get!(params[:ejercicio_id]).getMarcas()
-    #Comunicamos el resultado de la operacion y mandamos el json
-  end
-
-  put :modificarEjercicio, :map => '/rutinas/:rutina_id/ejercicios/:ejercicio_id/marcas/:marca_id' do
-    @marca = Marca.get!(params[:marca_id])
-    actualizar_marca(@marca,params)
-    #Comunicamos el resultado de la operacion y mandamos el json
-  end
-
-  delete :eliminar, :map => '/rutinas/:rutina_id/ejercicios/:ejercicio_id/marcas/:marca_id' do
-    @marca = Marca.get!(params[:marca_id])
-    @marca.destroy!()
     #Comunicamos el resultado de la operacion y mandamos el json
   end
 
--- app/helpers/MarcaHelper.rb
+++ app/helpers/MarcaHelper.rb
@@ -2,13 +2,9 @@ FitnessTimeApi::App.helpers do
 
 	def create_marca(jsonMarca, ejercicioDeCarga)
 
-		if(ejercicioDeCarga)
-    		ejercicio = EjercicioDeCarga.find_by_id(jsonMarca['idEjercicio'])
-    	else
-    		ejercicio = EjercicioDeAerobico.find_by_id(jsonMarca['idEjercicio'])
-    	end
     	marca = Marca.new
     	marca.ejercicio = ejercicio
+    	marca.idMobile = jsonMarca['idMobile']
     	marca.carga = jsonMarca['carga']
     	marca.fecha = jsonMarca['fecha']
     	marca.save()
