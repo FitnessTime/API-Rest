edf6c93d7242755acdd77653bdb2d1d01fff9447
skalic.julian <skalic.julian@gmail.com>
2016-06-26 20:08:08 -0300

    Agrego cerrar sesion

--- app/helpers/Mailer.rb
+++ app/helpers/Mailer.rb
@@ -15,7 +15,7 @@ FitnessTimeApi::App.helpers do
 	def enviar_mail_cambio_contrasenia(to, contrasenia)
 		begin
 		email(:from => "FitnessTime", :to => to, :subject => "Cambio password.",
-		 	  :body=>"Su nuevo password es: " + contrasenia
+		 	  :body=>"Su nuevo password es: " + contrasenia)
 		rescue Exception
 		end
 	end
