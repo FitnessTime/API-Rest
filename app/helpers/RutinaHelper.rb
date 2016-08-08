94fe9a228b7a2f805a7be52fd593f3b7dad68c22
skalic.julian <skalic.julian@gmail.com>
2016-08-08 18:32:00 -0300

    Fix estadisticas marcas

--- app/helpers/RutinaHelper.rb
+++ app/helpers/RutinaHelper.rb
@@ -54,7 +54,7 @@ FitnessTimeApi::App.helpers do
   end
 
   def retornar_rutinas_dto(securityToken)
-    rutinas = Rutina.find_all_by_usuario_email_and_eliminada(securityToken.emailUsuario, false)
+    rutinas = Rutina.find_all_by_usuario_email(securityToken.emailUsuario)
     ret_rutinas_dto = Array.new(rutinas.size)
     assembler = RutinaAssembler.new
     index = 0
