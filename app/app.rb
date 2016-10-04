7bdd555e21c8ed33b0321c640e5d233a8dc0c5c0
skalic.julian <skalic.julian@gmail.com>
2016-10-04 19:11:35 -0300

    Agrego metodo para eliminar usuarios

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,7 @@ module FitnessTimeApi
 
     set :delivery_method, :smtp => {
       :address              => "smtp.gmail.com",
+      :domain               => 'heroku.com',
       :port                 => 587,
       :user_name            => 'api.fitnesstime@gmail.com',
       :password             => '4demarzo',
